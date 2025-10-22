277ad942225b369189337b3a6a11691e311c15b0
Jevon Hill <jevon623@gmail.com>
2025-10-21 19:47:31 -0700

    Medical home footer removed.

--- components/usercharts/MedicalHomeBarChart.tsx
+++ components/usercharts/MedicalHomeBarChart.tsx
@@ -86,12 +86,12 @@ export function MedicalHomeBarChart({ casesPerFacility }: AnticipatedDispoArgume
                 )}
             </CardContent>
             <CardFooter className="flex-col items-start gap-2 text-sm">
-                <div className="flex gap-2 leading-none font-medium">
+                {/* <div className="flex gap-2 leading-none font-medium">
                     Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
                 </div>
                 <div className="text-muted-foreground leading-none">
                     Showing total visitors for the last 6 months
-                </div>
+                </div> */}
             </CardFooter>
         </Card>
     )
